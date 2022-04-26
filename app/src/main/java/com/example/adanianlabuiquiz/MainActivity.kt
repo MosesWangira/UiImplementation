5bad3a05117d73683a8529f81289ac9718b08f90
Moses Wangira <proudmoses@gmail.com>
2022-04-26 14:20:21 +0300

    Source code

---
+++ app/src/main/java/com/example/adanianlabuiquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.adanianlabuiquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
